a7dea317fd2b8ec7101a21e6f73c62a6156e4d1b
Никита Калугин <nikitakalugin02@gmail.com>
2022-10-21 14:14:02 +0700

    Fixex

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,33 @@ public class Main {
         int x0 = 0;
         double epsilon = 0.0001;
         double a = Integer.parseInt(args[0]);
-        double newX = function(x0, a);
+        double newX = reverseFunction(x0, a);
         double oldX = x0;
-        if (a == -1) {
+        if (a == 1) {
             System.out.println(0);
-        } else if (a > -1) {
+        } else if (a < 1) {
             System.out.println("I've got no roots, but my home was never on the ground");
         } else {
             while (Math.abs(newX - oldX) >= epsilon) {
                 oldX = newX;
-                newX = function(oldX, a);
+                newX = reverseFunction(oldX, a);
             }
+        }
+        System.out.println(newX);
+        newX = function (x0, a);
+        oldX = x0;
+        while(Math.abs(newX - oldX) >= epsilon) {
+            oldX = newX;
+            newX = function(oldX, a);
         }
         System.out.println(newX);
     }
 
-    public static double function(double x0, double a) {
-        return x0 - (1 / (Math.exp(Math.abs(a)) - 1)) * (Math.exp(x0) - x0 + a);
+    public static double reverseFunction(double x, double a){
+        return Math.log(x + a);
+    }
+
+    public static double function(double x, double a) {
+        return Math.exp(x) - a;
     }
 }
